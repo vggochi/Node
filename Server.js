a0e9ab21a2e14c4b33a128d2be2399ef77261613
vggochi <valentina.neves@aluno.senai.br>
2025-08-12 11:53:33 -0300

    1.0 Server.js
    Aprimorado

--- Server.js
+++ Server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const { param } = require('express/lib/application');
 const app = express();
+
 
 //Permitir receber dados em JSON
 app.use(express.json());
+
 
 //Simula um "banco de dados" em memória
 let produtos =[
@@ -10,12 +13,14 @@ let produtos =[
     {id: 2, nome:"Teclado"}
 ];
 
-//GET - Lista todos os prdutos
+
+//GET - Lista todos os produtos
 app.get('/api/produtos',(req,res)=>{
     res.json(produtos);
 });
 
-//Post
+
+//POST
 app.post('/api/produtos',(req,res)=>{
     const novoProduto ={
         id:produtos.length +1,
@@ -25,30 +30,74 @@ app.post('/api/produtos',(req,res)=>{
     res.status(201).json(novoProduto);
 })
 
-app.get('/', (req, res) =>{
-    res.send('Olá, este é o servidor com Express');
+
+//PUT
+app.put('/api/produtos/:id', (req,res)=>{
+    const id = parseInt(req.params.id, 10);
+    const produto = produtos.find(p=> p.id ===id);
+    if(isNaN(id)){
+        return res.status(400).json({ mensagem: 'O ID fornecido não é um número valido'});
+    }
+    if (!produto){
+        return res.status(404).json({mensagem:'produto não encotrado'});
+    }
+    const novoNome = req.body.nome;
+    if (!novoNome || novoNome.trim() === ''){
+        return res.status(400).json({mensagem:'O campo "nome" é obrigatório e não pode ser vazio'});
+    }
+    produto.nome =novoNome;
+    res.json(produto);
 });
 
-//rota sobre
+
+//DELETE
+app.delete('/api/produtos/:id', (req,res) => {
+    const id = parseInt(req.params.id, 10);
+    if (isNaN(id)) {
+        return res.status(400).json({mensagem:'O ID fornecido não é um número válido'})
+    }
+    const tamanhoOriginal = produtos.lenght;
+    produtos = produtos.filter(p => p.id !== id);
+    if (tamanhoOriginal === produtos.lenght){
+        return res.status(404).json({mensagem:'Produto não encontrado para exclusão'})
+    }
+    res.status(204).send();
+})
+
+
+
+//Rota principal
+app.get('/', (req, res) =>{
+    res.send('Olá, este é o servidor com Express');
+})
+
+
+//Rota "sobre"
 app.get('/sobre',(req,res)=>{
     res.send('Página Sobre');
 });
 
-//rota produtos
-app.get('/produtos',(req,res)=>{
+
+//Rota "produtos"
+app.get('/produtos', (req,res)=>{
     res.send('Lista de Produtos');
 });
 
-//rota que retorna JSON (simula uma API)
-app.get('/api/produtos',(req,res)=>{
+
+//Rota que retorna JSON (simula uma API)
+app.get('/api/produtos', (req,res)=>{
     const produtos = [
         {id: 1,nome:'Mouse'},
         {id: 2,nome:'Teclado'}
     ];
     res.json(produtos);
-});
+})
 
-//inicia o servidor na porta 3000
+
+//Inicia o servidor na porta 3000
 app.listen(3000, () =>{
     console.log('Servidor rodando http://localhost:3000');
 });
+
+
+//node server.js
